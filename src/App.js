adddc168eec8d23d378e82066fb4c9cb48e3e178
AlexeyVarenko <alexey_varenko@mail.ru>
2022-04-01 16:57:02 +0300

    Lesson 7
    Component?

--- src/App.js
+++ src/App.js
@@ -4,16 +4,36 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <div>One div</div>
-      <div className="App">
-        <ul>
-          <li>css</li>
-          <li>HTML</li>
-          <li>JS</li>
-        </ul>
-      </div>
+      <Header/ >
+      <Technologies />
     </div>
   );
 }
+
+
+const Technologies = ()=>{
+  return (
+    <div className="App">
+        <ul>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>JS</li> 
+        </ul>
+      </div>
+  );
+}
+
+const Header=()=>{
+  return (
+    <div>
+      <a href='#'>Home</a>
+      <a href='#'>News</a>
+      <a href='#'>Feed</a>
+    </div>
+  );
+}
+
+
+
 
 export default App;
