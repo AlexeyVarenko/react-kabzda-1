cab7ccb5421c2840ca1d99e57d1431ebb38f3521
AlexeyVarenko <alexey_varenko@mail.ru>
2022-04-02 16:26:29 +0300

    Lesson 12
    Components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <img src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80' />
+        <div> ava + description </div>
+        <div>
+          My posts
+          <div>
+            New Post
+          </div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+        <div>Post 3</div>
+        </div>
+      </div>
+    )
+}
+
+export default Profile
