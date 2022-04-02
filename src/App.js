18969e8bf31443baa0ccb12cab41a32789d69585
AlexeyVarenko <alexey_varenko@mail.ru>
2022-04-02 16:14:30 +0300

    Lesson 11
    grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
-import Header from './Header'
-import Technologies from './Technologies'
 
 const App = () => {
   return (
-    <div>
-      <Header/ >
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <img src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80' />
+        <div> ava + description </div>
+        <div>
+          My posts
+          <div>
+            New Post
+          </div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+        <div>Post 3</div>
+        </div>
+      </div>
     </div>
   );
 }
