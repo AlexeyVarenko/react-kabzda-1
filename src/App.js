1354c00dbbcf46037f7e8ab0b233e75d01e5645b
AlexeyVarenko <alexey_varenko@mail.ru>
2022-04-13 21:51:37 +0300

    Lesson 19
    Route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import News from './components/News/News';
 import Profile from './components/Profile/Profile';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      {/* <Profile />*/}
-      <div  className='app-wrapper-content'> 
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Routes>
+            < Route path="/dialogs" element={<Dialogs />} />
+            < Route path="/profile" element={<Profile />} />
+            < Route path="/news" element={<News />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 export default App;
