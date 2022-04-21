446cde96362eb782d754b4fb07c36d6184db2a4c
AlexeyVarenko <alexey_varenko@mail.ru>
2022-04-21 21:35:13 +0300

    Lesson 23
    props for Dialogs

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,15 +4,19 @@ import s from './MyPosts.module.css'
 
 const MyPosts = (props) => {
   return (
-    <div>
-      My posts
+    <div className={s.PostsBlock}>
+      <h3>My posts</h3>
       <div>
-        <textarea></textarea>
-        <button>Add post</button>
+        <div>
+          <textarea></textarea>
+        </div>
+        <div>
+          <button>Add post</button>
+        </div>
       </div>
       <div className={s.posts}>
-        <Post message='Hi! How are you?' likeCount='10'/>
-        <Post message="It's my first page" likeCount='15'/>
+        <Post message='Hi! How are you?' likeCount='10' />
+        <Post message="It's my first page" likeCount='15' />
       </div>
     </div>
   )
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import MyPosts from './MyPosts/MyPosts';
-import s from './Profile.module.css'
+import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
   return (
     <div>
-      <img src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80' />
-      <div> ava + description </div>
-      <MyPosts/>
+      <ProfileInfo />
+      <MyPosts />
     </div>
   )
 }
