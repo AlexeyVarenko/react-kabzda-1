8e70fc62642cfd9485e7948fef54aff0361ec0da
AlexeyVarenko <alexey_varenko@mail.ru>
2022-04-24 11:01:10 +0300

    Lesson 24
    UI-BLL

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import Post from './Post/Post';
 import s from './MyPosts.module.css'
 
 const MyPosts = (props) => {
+
+  let postData=[
+    {id:1, message:'Hi! How are you?', likeCount: 10},
+    {id:2, message:"It's my first page", likeCount: 15},
+]
+
   return (
     <div className={s.PostsBlock}>
       <h3>My posts</h3>
@@ -15,8 +21,8 @@ const MyPosts = (props) => {
         </div>
       </div>
       <div className={s.posts}>
-        <Post message='Hi! How are you?' likeCount='10' />
-        <Post message="It's my first page" likeCount='15' />
+        <Post message={postData[0].message} likeCount={postData[0].likeCount} />
+        <Post message={postData[1].message} likeCount={postData[1].likeCount} />
       </div>
     </div>
   )
