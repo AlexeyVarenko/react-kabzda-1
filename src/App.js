82b116759596d845051615e105a92526c6b7677f
AlexeyVarenko <alexey_varenko@mail.ru>
2022-04-28 11:00:51 +0300

    Lesson 28
    props index.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
 import Profile from './components/Profile/Profile';
 
-const App = () => {
+const App = (props) => {
+
+
+
   return (
     <BrowserRouter>
       <div className='app-wrapper'>
@@ -15,8 +18,8 @@ const App = () => {
         <Navbar />
         <div className='app-wrapper-content'>
           <Routes>
-            < Route path="/dialogs/*" element={<Dialogs />} />
-            < Route path="/profile/*" element={<Profile />} />
+            < Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>} />
+            < Route path="/profile/*" element={<Profile posts={props.posts}/>} />
             < Route path="/news/*" element={<News />} />
           </Routes>
         </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,14 +4,9 @@ import s from './MyPosts.module.css'
 
 const MyPosts = (props) => {
 
-  let posts=[
-    {id:1, message:'Hi! How are you?', likeCount: 10},
-    {id:2, message:"It's my first page", likeCount: 15},
-    {id:3, message:"BlaBla", likeCount: 11},
-    {id:4, message:"Yo", likeCount: 151},
-]
+ 
 
-let postsElement=posts.map(p=><Post message={p.message} likeCount={p.likeCount} />)
+let postsElement=props.posts.map(p=><Post message={p.message} likeCount={p.likeCount} />)
 
   return (
     <div className={s.PostsBlock}>
