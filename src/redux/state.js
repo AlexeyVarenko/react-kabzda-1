5e2ab4b400030347e00bc08ab06a5e023caf5363
AlexeyVarenko <alexey_varenko@mail.ru>
2022-05-19 21:56:45 +0300

    Lesson 40
    add Messages

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,9 +1,10 @@
 const ADD_POST='ADD-POST';
-const UPDATE_NEW_POST_TEXT='UPDATE-NEW-POST-TEXT'
+const UPDATE_NEW_POST_TEXT='UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY='UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE='SEND_MESSAGE';
 
 
 let store = {
-
     _state: {
 
         profilePage: {
@@ -31,6 +32,7 @@ let store = {
             { id: 4, message: 'Yo' },
             { id: 5, message: 'Yo' },
             ],
+            newMessageBody: ""
         }
     },
     _callSubscriber() {
@@ -55,6 +57,14 @@ let store = {
         } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
+        }  else if (action.type === 'UPDATE_NEW_MESSAGE_BODY') {
+            this._state.dialogsPage.newMessageBody=action.body;
+            this._callSubscriber(this._state);
+        } else if (action.type === 'SEND_MESSAGE') {
+            let body=this._state.dialogsPage.newMessageBody;
+            this._state.dialogsPage.newMessageBody = '';
+            this._state.dialogsPage.messages.push({id: 6, message: body});
+            this._callSubscriber(this._state);
         }
     }
 }
@@ -68,6 +78,18 @@ export const addPostActionCreator = () =>{
   export const updateNewPostTextActionCreator = (text) =>{
     return {
       type: UPDATE_NEW_POST_TEXT, newText: text
+    }
+  }
+
+  export const sendMessageCreator = () =>{
+    return {
+      type: SEND_MESSAGE
+    }
+  }
+
+  export const updateNewMessageBodyCreator = (body) =>{
+    return {
+      type: UPDATE_NEW_MESSAGE_BODY, body: body
     }
   }
   
