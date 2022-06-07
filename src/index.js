57dde373ee27bee397383b8ed35b7d182382b945
AlexeyVarenko <alexey_varenko@mail.ru>
2022-06-07 20:19:42 +0300

    lesson 47
    copy of ProfileReducer

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import store from "./redux/redux-store"
 import  { Provider } from 'react-redux';
 
-let rerenderEntireTree = (state) => {
+
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
@@ -15,14 +15,8 @@ let rerenderEntireTree = (state) => {
     </React.StrictMode>,
     document.getElementById('root')
   );
-}
 
-rerenderEntireTree(store.getState());
 
-store.subscribe( ()=>{
-  let state=store.getState();
-  rerenderEntireTree(state);
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,46 +1,51 @@
-const ADD_POST='ADD-POST';
-const UPDATE_NEW_POST_TEXT='UPDATE-NEW-POST-TEXT';
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 
-let initialState={
+let initialState = {
   posts: [
     { id: 1, message: 'Hi! How are you?', likeCount: 10 },
     { id: 2, message: "It's my first page", likeCount: 15 },
     { id: 3, message: "BlaBla", likeCount: 11 },
     { id: 4, message: "Yo", likeCount: 151 },
-],
+  ],
   newPostText: 'it-kamasutra.com',
 }
 
-const profileReducer= (state=initialState, action)=>{
-    switch (action.type) {
-    case ADD_POST:
-        let newPost = {
-            id: 5,
-            message: state.newPostText,
-            likeCount: 0,
-        }
-        state.posts.push(newPost);
-       state.newPostText = '';
-       return state;
-       case UPDATE_NEW_POST_TEXT:
-        state.newPostText = action.newText;
-        return state;
-        default:
-            return state;
+const profileReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST: {
+      let newPost = {
+        id: 5,
+        message: state.newPostText,
+        likeCount: 0,
+      };
+      let stateCopy = {...state };
+      stateCopy.posts = [...state.posts];
+      stateCopy.posts.push(newPost);
+      stateCopy.newPostText = '';
+      return stateCopy;
     }
+    case UPDATE_NEW_POST_TEXT: {
+      let stateCopy = { ...state };
+      stateCopy.newPostText = action.newText;
+      return stateCopy;
+    }
+    default:
+      return state;
+  }
 
 }
 
-export const addPostActionCreator = () =>{
-    return {
-      type: ADD_POST
-    }
+export const addPostActionCreator = () => {
+  return {
+    type: ADD_POST
   }
-  export const updateNewPostTextActionCreator = (text) =>{
-    return {
-      type: UPDATE_NEW_POST_TEXT, newText: text
-    }
+}
+export const updateNewPostTextActionCreator = (text) => {
+  return {
+    type: UPDATE_NEW_POST_TEXT, newText: text
   }
+}
 
 export default profileReducer;
