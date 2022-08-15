3d954e32be51f0637e087cd6a72072cb51618ea5
AlexeyVarenko <alexey_varenko@mail.ru>
2022-08-15 11:32:23 +0300

    Lesson 71
    setState, local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -46,5 +46,4 @@ let mapStateToProps = (state) => ({
 export default compose(
   connect (mapStateToProps, { getUserProfile }),
   withRouter,
-  withAuthRedirect
 )(ProfileContainer)
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Preloader from '../../Common/Preloader/preloader';
 import s from './ProfileInfo.module.css'
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -8,13 +9,16 @@ const ProfileInfo = (props) => {
   }
   return (
     <div>
+      
       <div>
         <img src='https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80' />
-        <div className={s.descriptionBlock}>
-          <img src={props.profile.photos.large} />
-          ava + description
-        </div>
       </div>
+      
+      <div className={s.descriptionBlock}>
+        <img src={props.profile.photos.large} />
+       <ProfileStatus status={"Hello my friends"}/>
+      </div>
+
     </div>
   )
 }
